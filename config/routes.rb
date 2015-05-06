638dccce6aacde92feb0e8fbebf247610a0c52de
Razvan Ciocanel <chocksy@gmail.com>
2015-05-06 13:24:24 +0300

    add routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'login' => 'sessions#new', :as => :login
+  get 'signup' => 'users#new', :as => :signup
+  delete 'logout' => 'sessions#destroy', :as => :logout
+  
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for "Member", at: 'auth'
