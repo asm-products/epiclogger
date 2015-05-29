3ebab21608bbc219c60a661ca7ad47be7976253c
Razvan Ciocanel <chocksy@gmail.com>
2015-05-29 13:49:23 +0300

    repo permissions requirement too

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile'
+  provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile,repo'
 end
