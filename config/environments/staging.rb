623c3986d29e96cc880b01bbf4cae6ffed5fd7d1
Razvan Ciocanel <chocksy@gmail.com>
2015-06-02 12:17:34 +0300

    added staging environment file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   config.action_mailer.default_url_options = { :host => ENV['MAILER_HOST'] }
   # config.action_mailer.delivery_method = :postmark
   # config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY']}
